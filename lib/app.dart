880ad22f40d75989ff579bc603dd4100fba969fb
Stanis <stanisvel.dev@gmail.com>
2021-08-21 17:47:05 +0200

    part1_course

---
+++ lib/app.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
+
+class CupertinoStoreApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+
+    return CupertinoApp(
+      theme: const CupertinoThemeData(brightness: Brightness.light),
+      home: CupertinoStoreHomePage(),
+    );
+  }
+}
+class CupertinoStoreHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+        middle: Text('Cupertino Store'),
+      ),
+      child: SizedBox(),
+    );
+  }
+}
