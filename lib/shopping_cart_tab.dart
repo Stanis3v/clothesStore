4660609a0975cd7c94fc95178fc15c0aafb7df8d
Stanis <stanisvel.dev@gmail.com>
2021-08-29 17:28:06 +0200

    part2_course

---
+++ lib/shopping_cart_tab.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/cupertino.dart';
+import 'package:provider/provider.dart';
+
+import 'model/app_state_model.dart';
+
+class ShoppingCartTab extends StatefulWidget {
+  @override
+  _ShoppingCartTabState createState() {
+    return _ShoppingCartTabState();
+  }
+}
+
+class _ShoppingCartTabState extends State<ShoppingCartTab> {
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<AppStateModel>(
+      builder: (context, model, child) {
+        return const CustomScrollView(
+          slivers: <Widget>[
+            CupertinoSliverNavigationBar(
+              largeTitle: Text('Shopping Cart'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
